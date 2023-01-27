848a1045c86321e332f8cac3caed3c4e3848ff23
Jose Soto <jsoto7087@gmail.com>
2023-01-27 06:05:08 -0500

    commit tree 12ecb2da90435866feb1c7aa09a894ecd1f5c143

--- config/environment.rb
+++ config/environment.rb
@@ -1,1 +1,7 @@
 # Set up Bundler here following the instructions in the README
+
+require 'bundler/setup'
+Bundler.require(:default, :development)
+
+#The two atguments that we are passign in to the #require method must be passed in th ecorrect order.
+#This code first requires in code from the Bundler gem, then uses Bundler.require to require all the gems listed in out gemfile! that saves us from needing to require each gem individually. 
